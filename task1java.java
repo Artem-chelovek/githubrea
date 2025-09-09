01b5a1678ce4fc58bdc854eaf416e18e9daae6be
artemmaiorv <61981536+Artem-chelovek@users.noreply.github.com>
2025-09-09 13:04:53 +0300

    task1java.java

--- task1java.java
+++ task1java.java
@@ -2,8 +2,9 @@
 //Создание списка на языке Java
 string[] li = new string[] {"a","1","b","2","c","3"};
 
-//Создание stack на языке Python
+//Создание stack на языке Java
 MyStack stack = new MyStack();
 stack.push("a");
 stack.push("b");
+
 stack.push("c");
